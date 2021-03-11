b3e01e834ad769c4429c8f1778667e1108d4106a
tohzen <melvin.l.mobley@gmail.com>
2021-03-11 15:19:55 -0500

    done

--- main.js
+++ main.js
@@ -1,5 +1,51 @@
 // Your code here.
 
+// Your code here.
+
+function getFirstName(str) {
+  return str.firstName
+
+}
+
+function getLastName(str) {
+  return str.lastName
+}
+
+function getFullName(str) {
+  return str.firstName + " " + str.lastName
+}
+
+function setFirstName(str, str2) {
+  str.firstName = str2; 
+}
+
+function setAge(str, str2) {
+  str.age = str2
+}
+
+function giveBirthday(num1) {
+  if(num1.age === undefined) {
+    return num1.age =1;
+  } else {
+    num1.age = num1.age +1;
+  }
+  }
+
+function marry(person1, person2) {
+  person1.married = true;
+  person2.married = true;
+  person1.spouseName = getFullName(person2);
+  person2.spouseName = getFullName(person1);
+  
+}
+
+function divorce(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+  delete person1.spouseName
+  delete person2.spouseName
+  
+}
 
 
 
